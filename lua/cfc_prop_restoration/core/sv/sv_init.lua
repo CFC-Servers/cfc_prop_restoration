3f454665946b5af64bf52fdbd26d9f552f6bdd9a
Bleck <OverlordBleck@gmail.com>
2020-02-27 16:57:28 -0600

    Use duplicator class instead

--- lua/cfc_prop_restoration/core/sv/sv_init.lua
+++ lua/cfc_prop_restoration/core/sv/sv_init.lua
@@ -27,69 +27,41 @@ local function isValidPlayer( ent )
     return IsValid( ent ) and ent:IsPlayer()
 end
 
-local function spawnInPlayerProps( sid64 )
-
+local function spawnInPlayerProps( ply )
+    local _ents = prop[ply:SteamID64()]
+    duplicator.Paste( ply, _ents[1], _ents[2] )
 end
-
-local function onEntCreated( ent )
-    if not IsValid( ent ) then return end
-    if ent:GetClass() ~= "prop_physics" then return end
-
-    local entOwner = ent:GetOwner()
-    if not isValidPlayer( entOwner ) then return end
-
-    --[[
-    What matters?
-     - constraints
-     - model
-     - position
-     - angle
-     - material
-     - color
-    ]]
-
-    local entity_data = {
-        model = ent:GetModel(),
-        pos = ent:GetPos(),
-        ang = ent:GetAngles(),
-        mat = ent:GetMaterial(),
-        col = ent:GetColor()
-        --constraints = ent:GetConstrainedEntities()
-    }
-
-    table.insert( prop_data[entOwner:SteamID64()], ent )
-end
-
-hook.Add( "OnEntityCreated", "CFC_Restoration_EntCreate", onEntCreated )
-
-local function onEntRemove( ent )
-    if not IsValid( ent ) then return end
-    if ent:GetClass() ~= "prop_physics" then return end
-
-    local entOwner = ent:GetOwner()
-    if not isValidPlayer( entOwner ) then return end
-
-    table.RemoveByValue( prop_data[entOwner:SteamID64()], ent )
-end
-
-hook.Add( "EntityRemoved", "CFC_Restoration_EntRemove", onEntRemove )
 
 local function handleReconnect( ply )
-
+    -- NET MESSAGE CLIENT HERE --
+    spawnInPlayerProps( ply )
 end
 
 hook.Add( "PlayerInitialSpawn", "CFC_Restoration_Reconnect", handleReconnect )
 
 local function handleDisconnect( ply )
-    recent_disconnects[ply:SteamID64()] = CurTime() + expire_time
+    local player_props = {}
+    local player_sid = ply:SteamID64()
+
+    for _, prop in pairs( ents.GetAll() ) do
+        if prop:CPPIGetOwner() ~= ply then continue end
+        if not duplicator.IsAllowed( prop:GetClass() ) then return end
+
+        table.insert( player_props, prop )
+    end
+
+    prop_data[player_sid] = duplicator.CopyEnts( player_props )
+    recent_disconnects[player_sid] = CurTime() + expire_time
 end
 
 hook.Add( "PlayerDisconnected", "CFC_Restoration_Disconnect", handleDisconnect )
 
-local function disconnectThink()
+local function restorationThink()
     -- Autosaving props
     if CurTime() >= next_save then
         savePropDataToFile()
+
+        next_save = CurTime() + autosave_delay
     end
 
     -- Deleting long disconnects
@@ -100,4 +72,4 @@ local function disconnectThink()
     end
 end
 
-hook.Add( "Tick", "CFC_Restoration_Tick", disconnectThink )
+hook.Add( "Tick", "CFC_Restoration_Tick", restorationThink )
