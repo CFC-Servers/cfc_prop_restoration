036689db84524dc3a8dd8da2e66a7a9ce0bd912f
Bleck <OverlordBleck@gmail.com>
2020-04-06 16:59:15 -0500

    Remove client side includes

--- lua/autorun/prop_restoration.lua
+++ lua/autorun/prop_restoration.lua
@@ -1,9 +1,4 @@
 if SERVER then
-    AddCSLuaFile( "cfc_prop_restoration/core/cl/cl_init.lua" )
     include( "cfc_prop_restoration/core/sv/sv_adinterface.lua" )
     include( "cfc_prop_restoration/core/sv/sv_init.lua" )
-end
-
-if CLIENT then
-    include( "cfc_prop_restoration/core/cl/cl_init.lua" )
 end
